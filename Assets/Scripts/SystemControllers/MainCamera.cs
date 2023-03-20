5bfafdca3972225c7fbca9dac620793887365990
Pranav C <xtreme5351@gmail.com>
2023-03-20 09:31:23 +0000

    Networking camera spawn fixed

--- Assets/Scripts/SystemControllers/MainCamera.cs
+++ Assets/Scripts/SystemControllers/MainCamera.cs
@@ -1,24 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Mirror;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace SystemControllers
 {
-    public class MainCamera : MonoBehaviour
+    public class MainCamera : NetworkBehaviour
     {
         private readonly float movementSpeed = 0.5f;
         private readonly float flySpeed = 0.1f;
         public GameObject self;
+        private int _sceneNo; 
         
         // Instantiation method
         private void Start()
         {
             // Set the camera object to the center of the simulation world.
             self.transform.position = new Vector3(135, 20, 60);
+            _sceneNo = SceneManager.GetActiveScene().buildIndex;
+            if (!isLocalPlayer && _sceneNo != 2)
+            {
+                self.gameObject.SetActive(false);
+            }
         }
 
         void Update()
         {
+            if (!isLocalPlayer && _sceneNo != 2)
+            {
+                return;
+            }
             KeyControls();
         }
 
