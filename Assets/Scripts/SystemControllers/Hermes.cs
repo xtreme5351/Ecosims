d69901d629767b46b5992f4e29b57bf8e4070353
Pranav C <xtreme5351@gmail.com>
2023-03-22 12:42:37 +0000

    Pause menu button added

--- Assets/Scripts/SystemControllers/Hermes.cs
+++ Assets/Scripts/SystemControllers/Hermes.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,11 +11,19 @@ namespace SystemControllers
         private static bool s_GameIsPaused;
         public GameObject pauseMenuUI;
         public GameObject mainMenu;
-        
+
+        public void Start()
+        {
+            s_GameIsPaused = false;
+        }
+
         void Update()
         {
+            // If the key pressed is the escape key, pause the game
+            // This condition is checked every frame by this object
             if (Input.GetKeyDown(KeyCode.Escape))
             {
+                // Boolean logic to toggle the main menu on and off based on the game state.
                 mainMenu.SetActive(s_GameIsPaused);
                 s_GameIsPaused = !s_GameIsPaused;
                 pauseMenuUI.SetActive(s_GameIsPaused);
@@ -23,9 +32,19 @@ namespace SystemControllers
 
         public void QuitSession()
         {
+            // Load the home page to exit the session
             SceneManager.LoadScene(0);
+        }
+
+        public void PauseGame()
+        {
+            mainMenu.SetActive(false);
+            pauseMenuUI.SetActive(true);
+            s_GameIsPaused = true;
         }
     }
    
 }
+
+
 
